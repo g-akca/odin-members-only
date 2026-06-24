2bd294c2c9841c8232fe69d8b89466d3040f07f2
g-akca <gok.akca66@gmail.com>
2026-06-24 19:07:32 +0300

    Add Tailwind CSS integration and update header.ejs

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
