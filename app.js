7693220d20a46959e712051152ddddc1d4f61a19
g-akca <gok.akca66@gmail.com>
2026-06-24 13:59:50 +0300

    Add index route to app, create initial index and header views

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.use(router);
 
 // Centralized error handler
