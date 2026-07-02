98d85940fba2c6b0811a6dc7bc5bcb266c4bc433
g-akca <gok.akca66@gmail.com>
2026-07-02 21:00:37 +0300

    Implement passport authentication with local strategy and bcrypt for password comparison

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 import express from "express";
 import session from "express-session";
 import passport from "passport";
+import bcrypt from "bcryptjs";
 import authRouter from "./routes/authRouter.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -19,6 +20,44 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+      
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" })
+      }
+
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
 
 // Routes
 app.get("/", (req, res) => {
